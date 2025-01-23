bf6549ab8f643f951f8e7f4f4f89b8c42cc269a3
Naqeeb-Butt <139251654+Naqeeb-Butt@users.noreply.github.com>
2025-01-24 01:49:41 +0500

    server.js

--- server.js
+++ server.js
@@ -94,6 +94,6 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, '0.0.0.0', () => { 
-  console.log(Server is running on port ${PORT});
+app.listen(49153, '0.0.0.0', () => { 
+  console.log(Server is running on port 49153);
 });
